9d075f8b181118847ba60c6c36e3437d9e42a114
kevin marinho <kevindmarinho4321@gmail.com>
2023-10-31 00:55:44 -0400

    Initial Commit

---
+++ Hallowedemy2/Hallowedemy2App.swift
@@ -0,0 +1,17 @@
+//
+//  Hallowedemy2App.swift
+//  Hallowedemy2
+//
+//  Created by kevin marinho on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Hallowedemy2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
